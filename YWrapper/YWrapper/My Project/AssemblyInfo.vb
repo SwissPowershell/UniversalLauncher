3ca79f78ccbe9fa2cf961ea229ab0ea5b7843fc5
SwissPowershell <Supreme@sefanet.ch>
2023-10-16 15:32:40 +0200

    Create AssemblyInfo.vb

---
+++ YWrapper/YWrapper/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("YWrapper")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Lombard Odier Darier Hentsch & Cie")> 
+<Assembly: AssemblyProduct("YWrapper")> 
+<Assembly: AssemblyCopyright("Copyright © Lombard Odier Darier Hentsch & Cie 2014")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("70962407-7ccb-4793-a513-c7fd7253a52a")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
